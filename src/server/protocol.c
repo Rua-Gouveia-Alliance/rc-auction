a42b6e9f8887ed188098b1927a9566dbdaff7a71
goncrust <goncalonrua@gmail.com>
2023-12-13 20:29:49 +0000

    small fix

--- src/server/protocol.c
+++ src/server/protocol.c
@@ -7,27 +7,27 @@
 #include <unistd.h>
 
 int interpret_req(char *msg) {
-    if (strncmp(msg, LIN_REQ, CODE_SIZE))
+    if (strncmp(msg, LIN_REQ, CODE_SIZE) == 0)
         return LIN;
-    else if (strncmp(msg, LOU_REQ, CODE_SIZE))
+    else if (strncmp(msg, LOU_REQ, CODE_SIZE) == 0)
         return LOU;
-    else if (strncmp(msg, UNR_REQ, CODE_SIZE))
+    else if (strncmp(msg, UNR_REQ, CODE_SIZE) == 0)
         return UNR;
-    else if (strncmp(msg, LMA_REQ, CODE_SIZE))
+    else if (strncmp(msg, LMA_REQ, CODE_SIZE) == 0)
         return LMA;
-    else if (strncmp(msg, LMB_REQ, CODE_SIZE))
+    else if (strncmp(msg, LMB_REQ, CODE_SIZE) == 0)
         return LMB;
-    else if (strncmp(msg, LST_REQ, CODE_SIZE))
+    else if (strncmp(msg, LST_REQ, CODE_SIZE) == 0)
         return LST;
-    else if (strncmp(msg, SRC_REQ, CODE_SIZE))
+    else if (strncmp(msg, SRC_REQ, CODE_SIZE) == 0)
         return SRC;
-    else if (strncmp(msg, CLS_REQ, CODE_SIZE))
+    else if (strncmp(msg, CLS_REQ, CODE_SIZE) == 0)
         return CLS;
-    else if (strncmp(msg, BID_REQ, CODE_SIZE))
+    else if (strncmp(msg, BID_REQ, CODE_SIZE) == 0)
         return BID;
-    else if (strncmp(msg, SAS_REQ, CODE_SIZE))
+    else if (strncmp(msg, SAS_REQ, CODE_SIZE) == 0)
         return SAS;
-    else if (strncmp(msg, OPA_REQ, CODE_SIZE))
+    else if (strncmp(msg, OPA_REQ, CODE_SIZE) == 0)
         return OPA;
     else
         return -1;
